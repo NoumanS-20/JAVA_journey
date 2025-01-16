0884cfef66a8138375440381b03f8e0d87d51bdf
NoumanS-20 <166951290+NoumanS-20@users.noreply.github.com>
2025-01-16 21:49:59 +0530

    added comments

--- PercentageCalculator.java
+++ PercentageCalculator.java
@@ -3,17 +3,17 @@ public class PercentageCalculator {
     public static void main(String[] args) {
         int percentage,science,maths,english,history,geography;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter marks of Science");
+        System.out.println("Enter marks of Science");//Taking science marks as input from user
         science = sc.nextInt();
-        System.out.println("Enter marks of Maths");
+        System.out.println("Enter marks of Maths");//Taking maths marks as input from user
         maths = sc.nextInt();
-        System.out.println("Enter marks of English");
+        System.out.println("Enter marks of English");//Taking english marks as input from user
         english = sc.nextInt();
-        System.out.println("Enter marks of History");
+        System.out.println("Enter marks of History");//Taking history marks as input from user
         history = sc.nextInt();
-        System.out.println("Enter marks of Geography");
+        System.out.println("Enter marks of Geography");//Taking geography marks as input from user
         geography = sc.nextInt();
-        percentage = (maths + english + geography + history + science)/5;
+        percentage = (maths + english + geography + history + science)/5;//Calculating percentage
         System.out.println(percentage);
     }
 
