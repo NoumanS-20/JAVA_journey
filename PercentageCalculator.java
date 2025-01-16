470ba7a88f27d337dbebc01928ff646fc50afcdc
NoumanS-20 <166951290+NoumanS-20@users.noreply.github.com>
2025-01-16 21:16:47 +0530

    code works and performs as expected

--- PercentageCalculator.java
+++ PercentageCalculator.java
@@ -1,8 +1,20 @@
 import java.util.Scanner;
 public class PercentageCalculator {
     public static void main(String[] args) {
+        int percentage,science,maths,english,history,geography;
         Scanner sc = new Scanner(System.in);
-        System.out.println(a);
+        System.out.println("Enter marks of Science");
+        science = sc.nextInt();
+        System.out.println("Enter marks of Maths");
+        maths = sc.nextInt();
+        System.out.println("Enter marks of English");
+        english = sc.nextInt();
+        System.out.println("Enter marks of History");
+        history = sc.nextInt();
+        System.out.println("Enter marks of Geography");
+        geography = sc.nextInt();
+        percentage = (maths + english + geography + history + science)/5;
+        System.out.println(percentage);
     }
 
     
