beb5f2b39f2db27c543c62a1de614fee94b3377f
NoumanS-20 <166951290+NoumanS-20@users.noreply.github.com>
2025-01-16 21:11:37 +0530

    clean up

--- PercentageCalculator.java
+++ PercentageCalculator.java
@@ -1,10 +1,7 @@
 import java.util.Scanner;
 public class PercentageCalculator {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println();
         Scanner sc = new Scanner(System.in);
-        boolean a = sc.hasNextInt();
         System.out.println(a);
     }
 
