797813a2b16981dff3fffe8a648040b4eb037067
NoumanS-20 <166951290+NoumanS-20@users.noreply.github.com>
2025-03-01 18:17:07 +0530

    Added missing brackets in the if statements so that proper precedence of operators takes place

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -7,10 +7,10 @@ class CHESSBOARD {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
         char[] str = s.toCharArray();
-        if((str[0]=='a' || str[0]=='c' ||str[0]=='e' ||str[0]=='g') && (str[1]=='1' ||str[1]=='3' ||str[1]=='5' ||str[1]=='7')){
+        if(str[0]=='a' || str[0]=='c' ||str[0]=='e' ||str[0]=='g' && str[1]=='1' ||str[1]=='3' ||str[1]=='5' ||str[1]=='7'){
             System.out.println("Black");
         }
-        else if((str[0]=='b' || str[0]=='d' ||str[0]=='f' ||str[0]=='h') && (str[1]=='2' ||str[1]=='4' ||str[1]=='6' ||str[1]=='8')){
+        else if(str[0]=='b' || str[0]=='d' ||str[0]=='f' ||str[0]=='h' && str[1]=='2' ||str[1]=='4' ||str[1]=='6' ||str[1]=='8'){
             System.out.println("Black");
         }
         else{
